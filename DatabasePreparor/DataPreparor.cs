de68f7cccfbe3c7511e0e8144192c0ccf6697f19
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2023-03-11 16:49:26 +0200

    databe automatic migration

---
+++ DatabasePreparor/DataPreparor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineNewsPaper.Data;
+
+namespace OnlineNewsPaper.DatabasePreparor
+{
+    public static class DataPreparor
+    {
+        public static IApplicationBuilder Preparo(this IApplicationBuilder app)
+        {
+            using var scopedService = app.ApplicationServices.CreateScope();
+            var serviceProvider = scopedService.ServiceProvider;
+            var db = serviceProvider.GetRequiredService<ApplicationDbContext>();
+
+            db.Database.Migrate();
+
+            return app;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using OnlineNewsPaper.Data;
+using OnlineNewsPaper.DatabasePreparor;
+using static OnlineNewsPaper.DatabasePreparor.DataPreparor;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,6 +29,8 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.Preparo(); //migrate Database befroe execute!
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
