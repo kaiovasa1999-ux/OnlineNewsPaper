e70c5d4ff1e0fc3b9c2992121ed49032adf752f2
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2023-03-19 16:30:27 +0200

    categories api get-post

---
+++ Controllers/API/CategoriesApiController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineNewsPaper.Data;
+using OnlineNewsPaper.Data.Models;
+
+namespace OnlineNewsPaper.Controllers.API
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext db;
+        public CategoriesApiController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet] 
+        public ActionResult<IEnumerable<NewsCategory>> Get()
+        {
+            return this.db.NewsCategories.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<NewsCategory> Get(int id)
+        {
+            var newsCategory =this.db.NewsCategories.SingleOrDefault(c => c.Id == id);
+            if(newsCategory == null)
+            {
+                return NotFound();
+            }
+            return newsCategory;
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post(NewsCategory newsCategory)
+        {
+            await this.db.NewsCategories.AddAsync(newsCategory);
+            await this.db.SaveChangesAsync();
+
+            return this.CreatedAtAction("Get", new { newsCategory.Id }, newsCategory);
+        }
+    }
+}
