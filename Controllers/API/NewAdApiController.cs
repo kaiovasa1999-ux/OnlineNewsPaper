ae438d9cf83a02bc1f67d3ab5b84f81460614aca
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2023-03-19 16:09:45 +0200

    nesAD api get

---
+++ Controllers/API/NewAdApiController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineNewsPaper.Data;
+using OnlineNewsPaper.Data.Models;
+
+namespace OnlineNewsPaper.Controllers.API
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NewAdApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext db;
+
+        public NewAdApiController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public ActionResult<NewsAd> GetNewsAd()
+        {
+            var newsAd = new NewsAd
+            {
+                Title = "kaivoasa",
+                Dislike = 3,
+                Likes = 4,
+            };
+
+            return newsAd;
+        }
+    }
+}
